bb907e1e5700a4bddcf5b272997531828a5ddb5f
jrom <jrom@gondor.local>
2012-05-25 16:38:06 +0200

    * LayerDescription : set polygon opacity to 0.4 by default

--- client/js/mapshup/lib/core/Map/LayerDescription.js
+++ client/js/mapshup/lib/core/Map/LayerDescription.js
@@ -190,7 +190,7 @@
 
             var icon = null, // Determines if point must be represented by an icon
                 obj = this.obj, // Object reference
-                opacity = obj.opacity || 0; // Set opacity - 0.4 by default
+                opacity = obj.opacity || 0.4; // Set opacity - 0.4 by default
 
             /*
              * Features got a 'icon' attribute => use it as symbol
