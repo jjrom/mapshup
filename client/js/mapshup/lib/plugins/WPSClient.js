d446071c7faa77f23fe35921cea88a466fdd59cb
jerome.gasperi@cnes.fr <>
2012-11-30 16:21:36 +0100

    * WPSClient : make WPS layer editable

--- client/js/mapshup/lib/plugins/WPSClient.js
+++ client/js/mapshup/lib/plugins/WPSClient.js
@@ -1211,6 +1211,7 @@
                 type:"GeoJSON",
                 title:"WPS results",
                 clusterized:false, // Very important !
+                editable:true,
                 ol:{
                     styleMap: new OpenLayers.StyleMap({
                         'default':{
