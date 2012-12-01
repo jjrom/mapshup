ad8c83877e8b74c89dbdcbefb5f4580fb7efcd6e
jjrom <jerome.gasperi@gmail.com>
2012-12-01 21:23:04 +0100

    * Merge Toolbar and ToolbarItem within one single Toolbar.js file

--- client/js/mapshup/lib/core/Toolbar.js
+++ client/js/mapshup/lib/core/Toolbar.js
@@ -188,9 +188,9 @@
             var tbItem, self = this;
             
             /*
-             * Create a ToolbarItem
+             * Create a Toolbar Item
              */
-            tbItem = new M.ToolbarItem(self, item);
+            tbItem = new M.Toolbar.Item(self, item);
             
             /*
              * Add a new item
@@ -205,7 +205,7 @@
         };
 
         /*
-         * Activate ToolbarItem identified by id
+         * Activate Toolbar Item identified by id
          * 
          * @param id : item to activate/deactivate
          * @param activate: true to activate, false to deactivate
@@ -221,7 +221,7 @@
         };
         
         /*
-         * Return ToolbarItem identified by id
+         * Return Toolbar Item identified by id
          */
         this.get = function(id) {
             for (var i = 0, l = this.items.length ; i < l; i++) {
@@ -233,7 +233,7 @@
         };
         
         /*
-         * Remove ToolbarItem
+         * Remove Toolbar Item
          */
         this.remove = function(id) {
             
@@ -249,6 +249,264 @@
            
         };
         
+        /*
+         * Initialize object
+         */
+        return this.init();
+      
+    };
+
+    /*
+     * Toolbar Item options
+     * 
+     *  {
+     *      activable: // boolean - if true, click on button set or unset 'activate' class
+     *                        (default true)
+     *      callback: // function to call on click
+     *      e: // Extras properties - Properties under this property can be anything
+     *      first:// boolean - if true item is added as the first element of the toolbar
+     *                       - if false item is added at the end of the toolbar
+     *                       (default false)
+     *      html: // html code to display within the button instead of title - If both html and title
+     *               are specified, html has preseance (i.e. title is discarded)
+     *      icon: // Url to the icon image (if no text)
+     *      id: // Unique identifier for this item
+     *      nohover: // if true, item is not sensitive to onmouseover event
+     *      switchable: // boolean - if true, click on item alternate activate/deactivate
+     *                             - if false, click on item always activate it
+     *                             (default true)
+     *      title: // Text displayed within the item display (if no icon specifified)
+     *      tt: // Text displayed on mouse over
+     *      scope: // reference to the calling plugin
+     * }
+     *
+     */
+    M.Toolbar.Item = function(tb, options) {
+        
+        /*
+         * Paranoid mode
+         */
+        options = options || {};
+        
+        /*
+         * If true, click on button make it active
+         */
+        this.activable = options.hasOwnProperty("activable") ? options.activable : true;
+        
+        /*
+         * Callback function called on click
+         */
+        this.callback = options.callback;
+         
+        /*
+         * Extra properties container
+         */
+        this.e = options.e || {};
+        
+        /*
+         * Extra properties container
+         */
+        this.first = M.Util.getPropertyValue(options, "first", false);
+        
+        /*
+         * Html content for the button - replace title
+         */
+        this.html = options.html;
+        
+        /*
+         * Url to the button icon image 
+         */
+        this.icon = options.icon;
+        
+        /*
+         * Unique identifier for this element. Automatically created if not given
+         * !! THIS ID IS DIFFERENT FROM THE ID OF THE CREATED jquery $d ELEMENT !!
+         */
+        this.id = options.id || M.Util.getId();
+       
+        /*
+         * Boolean. If true, button is not sensitive to onmouseover event
+         */
+        this.nohover = options.nohover || false;
+        
+        /*
+         * If true, click on button make alternate activate/deactivate. If false, click
+         * on button make it active
+         */
+        this.switchable = options.hasOwnProperty("switchable") ? options.switchable : true;
+        
+        /*
+         * Toolbar reference 
+         */
+        this.tb = tb;
+        
+        /*
+         * Textual content of the button
+         */
+        this.title = options.title;
+        
+        /*
+         * Title to display on tooltip 
+         */
+        this.tt = options.tt || "";
+        
+        /*
+         * Plugin scope reference
+         */
+        this.scope = options.scope;
+         
+        /*
+         * Initialize Item
+         */
+        this.init = function() {
+
+            var orientation,uid,content,p,self = this;
+            
+            /*
+             * No toolbar - no Toolbar Item
+             */
+            if (!self.tb) {
+                return null;
+            }
+            
+            /*
+             * Set extras properties
+             */
+            for (p in self.e) {
+                if (self.e.hasOwnProperty(p)) {
+                    self[p] = self.e[p];
+                }
+            }
+
+            /*
+             * Delete self.e
+             */
+            delete self.e;
+            
+            /*
+             * Add a <li> element to toolbar
+             * If button 'first' property is set to true,
+             * the button is added at the beginning of the toolbar
+             * otherwise it is added at the end of the toolbar
+             */
+            uid = M.Util.getId();
+            
+            if (self.html) {
+                content = self.html;
+            }
+            else {
+                content = self.title ? M.Util.shorten(self.title,10,true) : '<img class="middle" alt="" src="'+M.Util.getImgUrl(self.icon || "empty.png")+'"/>';
+            }
+            content = '<div class="'+(self.nohover ? "" : "hover ")+'item" jtitle="'+(M.Util._(self.tt) || "")+'" id="'+uid+'">'+content+'</div>';
+            self.first ? self.tb.$d.prepend(content) : self.tb.$d.append(content);
+            
+            /*
+             * Get newly created div reference
+             */
+            self.$d = $('#'+uid);
+            
+            /*
+             * Define an action to the <li> element
+             */
+            self.$d.click(function(){
+
+                /*
+                 * Activate or deactivate button
+                 * Button with 'switchable' set to false are always set to active
+                 */
+                self.switchable ? self.activate(!self.$d.hasClass('active')) : self.activate(true);
+                
+                /*
+                 * Callback is defined
+                 */
+                if ($.isFunction(self.callback)) {
+
+                    /*
+                     * Return the scope and the clicked <li> to the callback function
+                     */
+                    self.callback(self.scope, self);
+                }
+
+                return false;
+
+            });
+                
+            /*
+             * Add a tooltip depending on orientation
+             */
+            if (self.tt) {
+                orientation = self.tb.orientation || 'h';
+                if (self.tb.position) {
+                    M.tooltip.add(self.$d, orientation === 'h'? self.tb.position.substr(0,1) : self.tb.position.substr(1,2));
+                }
+                else {
+                    M.tooltip.add(self.$d, orientation === 'h'? 'n' : 'e');
+                }
+            }
+            
+            return this;
+        };
+        
+        /*
+         * Trigger the button
+         */
+        this.trigger = function() {
+            this.$d.trigger('click');
+        };
+        
+        /*
+         * Activate or deactivate item
+         * 
+         * @param b : boolean - true to activate, false to deactivate
+         */
+        this.activate = function(b) {
+            
+            var i,l,item,self = this;
+            
+            /*
+             * Do nothing if item is not activable
+             */
+            if (!self.activable) {
+                return false;
+            }
+            
+            /*
+             * Activate item
+             */
+            if (b) {
+                
+                /*
+                 * Remove all 'active' class from toolbar items
+                 * This step is only applied to activable items
+                 */
+                for (i = 0, l = self.tb.items.length; i < l; i++) {
+                    item = self.tb.items[i];
+                    if (item.id !== self.id) {
+                        if (item.activable) {
+                            item.$d.removeClass('active');
+                        }
+                    }
+                }
+                
+                /*
+                 * Add 'active' class to selected item
+                 */
+                self.$d.addClass("active");
+                
+            }
+            else {
+                
+                /*
+                 * Remove 'active' class from item
+                 */
+                self.$d.removeClass("active");
+                
+            }
+            
+            return true;
+            
+        };
+
         /*
          * Initialize object
          */
