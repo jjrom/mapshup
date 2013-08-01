7a66cf6fe623aacfdbffafb618be652f32bc45dc
jjrom <jerome.gasperi@gmail.com>
2013-08-01 17:09:30 +0200

    UTFGrid - complete rewrite. Mouse pointer set as a cursor when hover UTFGrids data

--- client/js/mapshup/lib/plugins/UTFGrid.js
+++ client/js/mapshup/lib/plugins/UTFGrid.js
@@ -39,251 +39,232 @@
  * UTFGrid plugin.
  */
 (function(M) {
-    
+
     M.Plugins.UTFGrid = function() {
-        
+
         /*
          * Only one UTFGrid object instance is created
          */
         if (M.Plugins.UTFGrid._o) {
             return M.Plugins.UTFGrid._o;
         }
-        
+
         /*
          * Active status
          */
         this.active = false;
-        
+
         /*
          * Store last hilited grid object
          */
         this._current = {};
-        
+
         /*
          * List of UTFGrid layers
          */
         this.layers = [];
-        
+
         /*
          * True to display geometry if available
          */
-        this.useGeo = true;
-        
+        this.useGeo = false;
+
         /*
          * Init plugin
          */
         this.init = function(options) {
-            
+
             var self = this;
-            
+
             /**
              * Init options
              */
             self.options = options || {};
 
             /*
-             * Create UTFGrid alert container
-             */
-            self.$d = M.Util.$$('#'+M.Util.getId()).addClass("utfginfo").click(function(){
-                self.activate(!self.active);
-            });
-            
-            /*
-             * Define action on mousemove
-             */
-            M.Map.map.events.register('mousemove', M.Map.map, function(e){
-                
-                /*
-                 * Never display UTFGrid info if a vector feature is already hilited
-                 */
-                if (M.Map.$featureHilite.attr("hilited") === "hilited") {
-                    
-                    /* Important - reset last hilited object if any */
-                    self._current = {};
-                    
-                    return false;
-                }
-                
-                var i, l, items, layer, z, mz,
-                show = false,
-                lonLat = M.Map.map.getLonLatFromPixel(M.Map.mousePosition);
-                
-                if (!lonLat) { 
-                    return false;
-                }    
-                
-                /*
-                 * Roll over UTFGrid layers
-                 */
-                if (self.layers.length > 0) {
-                    items = {};
-                    for (i = 0, l = self.layers.length; i<l; i++) {
-                        
-                        layer = self.layers[i];
-                        
-                        /*
-                         * Do not send request outside of zoom levels and location bounds
-                         */
-                        z = layer["_M"].z;
-                        mz = M.Map.map.getZoom();
-                        
-                        if ((mz >= z[0] && mz <= z[1]) && layer["_M"].bounds.containsLonLat(lonLat)) {
-                            
-                            /*
-                             * Show info if UTFGrid is active
-                             */
-                            if (self.active) {
-                                items[OpenLayers.Util.indexOf(M.Map.map.layers, layer)] = {
-                                    attributes:layer.getFeatureInfo(lonLat),
-                                    modifiers:layer["_M"].layerDescription["info"]
-                                };
-                            }
-                            
-                            show = true;
-                            
-                        }
-                        
-                    }
-                    
-                    /*
-                     * Show or hide UTFGrid alert container depending on UTFGrid data are present or not
-                     */
-                    show ? self.$d.show() : self.$d.hide();
-                    
-                    self.getInfo(items);
-                    
-                }
-                
-                /*
-                 * Activate or deactivate UTFGrid alert during startup
-                 */
-                self.activate(self.active);
-                
-                return true;
-                
-            });
-            
-            /*
-             * Create overlay layer to display polygon on hover
-             */
-            self.layer = new OpenLayers.Layer.Vector("__LAYER_UTFGRID__", {
-                projection:M.Map.sm,
-                displayInLayerSwitcher:false,
-                styleMap:new OpenLayers.StyleMap({
-                    'default':{
-                        strokeColor:'white',
-                        strokeWidth: 1,
-                        fillColor:'yellow',
-                        fillOpacity: 0.2
-                    }
-                })
-            });
-
-            /**
-             * Add Drawing layer to Map object
-             */
-            M.Map.addLayer({
-                type:"Generic",
-                title:self.layer.name,
-                unremovable:true,
-                MLayer:true,
-                layer:self.layer
-            });
-            
-            /*
              * Track layersend events to add/remove UTFGrid layers
              */
             M.Map.events.register("layersend", self, function(action, layer, scope) {
-                
-                var i,l;
-                
+
+                var i, l;
+
                 /*
                  * Only process UTFGrid layers
                  */
                 if (!layer || !layer["_M"] || !layer["_M"].layerDescription) {
                     return false;
                 }
-                
+
                 if (layer["_M"].layerDescription["type"] !== "UTFGrid") {
                     return false;
                 }
-                
+
                 if (action === "add") {
                     scope.layers.push(layer);
                 }
                 else if (action === "remove") {
-                    for (i = 0, l = scope.layers.length ; i < l; i++) {
+                    for (i = 0, l = scope.layers.length; i < l; i++) {
                         if (scope.layers[i].id === layer.id) {
-                            scope.layers.splice(i,1);
+                            scope.layers.splice(i, 1);
                             break;
                         }
                     }
                 }
+
+                return true;
+
+            });
+
+            /*
+             * Show pointer on mousemove
+             */
+            M.Map.map.events.register('mousemove', M.Map.map, function(e) {
+
+                /*
+                 * If UTFGrid is deactivated do nothing
+                 */
+                if (!self.active) {
+                    return false;
+                }
+
+                /*
+                 * Never display UTFGrid info if a vector feature is already hilited
+                 */
+                if (M.Map.$featureHilite.attr("hilited") === "hilited") {
+
+                    /* Important - reset last hilited object if any */
+                    self._current = {};
+
+                    return false;
+                }
+
+                var data = self.getData(M.Map.map.getLonLatFromPixel(M.Map.mousePosition));
+
+                data.length > 0 ? M.$map.addClass('pointer') : M.$map.removeClass('pointer');
+                self.getInfo(data);
                 
                 return true;
+
+            });
+
+            /*
+             * Track layersend events to add/remove UTFGrid layers
+             */
+            M.Map.events.register("mapclicked", self, function(event, scope) {
+                
+                var bbox, data = scope.getData(M.Map.map.getLonLatFromPixel(M.Map.mousePosition));
+                
+                /*
+                 * Zoom on first object if bbox is defined
+                 */
+                if (data.length > 0) {
+                    bbox = data[0].attributes.data.BBOX ||  data[0].attributes.data.bbox;
+                    if (bbox) {
+                        M.Map.map.zoomToExtent(M.Map.Util.getProjectedBounds(bbox));
+                    }
+                }
                 
             });
-            
+
+            /*
+             * Activate UTFGrid during startup
+             */
+            self.activate(true);
+
             return self;
-            
+
         };
-        
+
         /*
          * Activate or deactivate UTFGrid detection
          */
         this.activate = function(b) {
             this.active = b;
-            this.$d.html(M.Util._((b ? "Hide" : "Show") +" thematic metadata"));
             if (!b) {
-                this.layer.destroyFeatures();
+                this.activateGeo(false);
             }
+            return b;
         };
-        
+
         /*
          * Activate or deactivate UTFGrid detection
          */
         this.activateGeo = function(b) {
-            this.useGeo = b;
-            if (!b) {
-                this.layer.destroyFeatures();
+
+            var self = this;
+
+            /*
+             * Create overlay polygon layer to display geometry on hover
+             */
+            if (b && !self.layer) {
+
+                self.layer = new OpenLayers.Layer.Vector("__LAYER_UTFGRID__", {
+                    projection: M.Map.sm,
+                    displayInLayerSwitcher: false,
+                    styleMap: new OpenLayers.StyleMap({
+                        'default': {
+                            strokeColor: 'white',
+                            strokeWidth: 1,
+                            fillColor: 'yellow',
+                            fillOpacity: 0.2
+                        }
+                    })
+                });
+
+                /**
+                 * Add Drawing layer to Map object
+                 */
+                M.Map.addLayer({
+                    type: "Generic",
+                    title: self.layer.name,
+                    unremovable: true,
+                    MLayer: true,
+                    layer: self.layer
+                });
             }
+
+            /*
+             * Empty overlay polygon layer
+             */
+            if (!b && self.layer) {
+                self.layer.destroyFeatures();
+            }
+
+            self.useGeo = b;
+
+            return b;
+
         };
-        
+
         /*
          * Display items info
          * 
-         * @param {Array} items : array of item
-         *          {
-         *              attributes: // object of attributes
-         *                  { data://, id://}
-         *              modifiers: // object modifier
+         * @param {Array} data : structure is
+         *              [
          *                  {
-         *                      title: // title to be displayed
-         *                      keys:{
-         *                          '...':{
-         *                              transform:function()
-         *                          }
+         *                      layer: // The parent UTFGrid layer
+         *                      attributes: {
+         *                          id: // This is the key identifier 
+         *                          data: // This is the 'data' bloc read within the UTFGrid                              
          *                      }
-         *                  }
-         *              
-         *          }
+         *                  },
+         *                  ...
+         *              ]
          */
-        this.getInfo = function(items) {
-            
+        this.getInfo = function(data) {
+
             var k, keys, item, o, c = "", features = [], self = this;
-            
-            items = items || {};
-            
+
             /*
-             * Roll over items
+             * Roll over data
              */
-            for (item in items) {
-                
-                o = items[item];
-                o.attributes  = o.attributes || {};
-                o.modifiers  = o.modifiers || {};
+            for (item in data) {
+
+                o = data[item];
+                o.attributes = o.attributes || {};
+                o.modifiers = o.layer["_M"].layerDescription["info"] || {};
                 keys = o.attributes.data || null;
-                
+
                 if (keys) {
 
                     /*
@@ -295,17 +276,17 @@
                         M.Map.$featureHilite.show();
                         return false;
                     }
-                    
+
                     if (o.modifiers.title) {
                         c = M.Util.parseTemplate(o.modifiers.title, keys, o.modifiers.keys);
                     }
                     else {
 
                         /*
-                        * Roll over data key
-                        */
+                         * Roll over data key
+                         */
                         for (k in keys) {
-                            c += '<p>'+k+' : ' + keys[k] + '</p>';
+                            c += '<p>' + k + ' : ' + keys[k] + '</p>';
                         }
 
                     }
@@ -316,46 +297,110 @@
                     if (self.useGeo && keys["wkt"]) {
                         features.push(new OpenLayers.Feature.Vector(OpenLayers.Geometry.fromWKT(keys["wkt"])));
                     }
-                    
+
                     self._current = keys;
                 }
-   
+
             }
-            
+
             if (c !== "") {
-               
+
                 /*
-                * Display tooltip
-                */
+                 * Display tooltip
+                 */
                 M.Map.$featureHilite.html(c).show();
-                
+
                 /*
-                * Display geometry
-                */
-                if (features.length > 0) {
+                 * Display geometry
+                 */
+                if (self.layer && features.length > 0) {
                     self.layer.destroyFeatures();
                     self.layer.addFeatures(features);
                 }
-                
+
                 return true;
             }
-            
+
             /*
              * Hide container
              */
-            self.layer.destroyFeatures();
+            if (self.layer) {
+                self.layer.destroyFeatures();
+            }
             self._current = {};
             M.Map.$featureHilite.hide();
-            
+
             return false;
-            
+
         };
-        
+
+        /*
+         * Return an array of data under the mouse pointer.
+         * Data are read from all mapshup UTFGrids layers
+         * 
+         * @param {OpenLayers.LonLat} lonLat
+         * @return {Array} data : structure is
+         *              [
+         *                  {
+         *                      layer: // The parent UTFGrid layer
+         *                      attributes: {
+         *                          id: // This is the key identifier 
+         *                          data: // This is the 'data' bloc read within the UTFGrid                              
+         *                      }
+         *                  },
+         *                  ...
+         *              ]
+         */
+        this.getData = function(lonLat) {
+
+            var i, l, z, mz, attributes, layer, data = [];
+
+            /*
+             * If UTFGrid is deactivated do nothing
+             */
+            if (!lonLat) {
+                return data;
+            }
+
+            /*
+             * Roll over UTFGrid layers
+             */
+            for (i = 0, l = this.layers.length; i < l; i++) {
+
+                layer = this.layers[i];
+
+                /*
+                 * Do not send request outside of zoom levels and location bounds
+                 */
+                z = layer["_M"].z;
+                mz = M.Map.map.getZoom();
+                if ((mz >= z[0] && mz <= z[1]) && layer["_M"].bounds.containsLonLat(lonLat)) {
+
+                    attributes = layer.getFeatureInfo(lonLat);
+
+                    /*
+                     * Only return true data (i.e. if key id is "" there is no data)
+                     */
+                    if (attributes && attributes.id) {
+                        data.push({
+                            layer: layer,
+                            attributes: attributes
+                        });
+                    }
+
+                }
+
+            }
+
+            return data;
+
+        };
+
         /*
          * Set unique instance
          */
         M.Plugins.UTFGrid._o = this;
-        
+
         return this;
     };
 })(window.M);
