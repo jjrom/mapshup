821d36119f98e77b6f1fe6419d7d057cae4095c4
jerome.gasperi@cnes.fr <>
2013-12-06 16:02:30 +0100

    Update resto.html sandbox example

--- client/sandbox/config/resto.js
+++ client/sandbox/config/resto.js
@@ -7,12 +7,19 @@
     c["general"].serverRootUrl = 'http://localhost/mapshups';
     c["general"].indexPath = "/sandbox/es.html";
     c["general"].confirmDeletion = false;
-    
+    c["general"].themePath = "/js/mapshup/theme/blacker";
     /*
      * Remove layers
      */
     c.remove("layers","OpenStreetMap");
     c.remove("layers","MapQuest OSM");
+    c.remove("plugins","UserManagement");
+    c.remove("plugins","UTFGrid");
+    c.remove("plugins","Welcome");
+    c.remove("plugins","WorldGrid");
+    c.remove("plugins","Slacker");
+    c.remove("plugins","UserManagement");
+    
     
     c["general"].timeLine = {
         enable:false
